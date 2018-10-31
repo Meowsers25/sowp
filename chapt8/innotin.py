8ce7b2947f7045ef68e3589971831e58e41cea53
Meowsers25 <c.lamothe25@gmail.com>
2018-10-30 23:42:00 -0400

    commit

---
+++ chapt8/innotin.py
@@ -0,0 +1,18 @@
+names = ['Luna', 'Beatrice', 'Sully']
+names.append('Katie')
+names.append('Chris')
+print(names)
+
+
+if 'Sully' in names:
+    print('Hi Sully')
+else:
+    print('Sully is MIA')
+if 'Katie' in names:
+    print('Hi sexy')
+else:
+    print('Awwwww')
+if 'Chris' not in names:
+    print('Where\'s the best guy in the world?')
+else:
+    print('Hey, Awesome guy!')
